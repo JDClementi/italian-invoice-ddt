99a9e3d447c125dd08b7844928467098e5c4ffbb
Jacopo Di Clementi <j.diclementi@araneum.it>
2018-01-17 20:58:35 +0100

    Primo commir

---
+++ src/ItalianInvoiceDdt.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jacopo Di Clementi
+ * Date: 17/01/2018
+ * Time: 17:10
+ * Project: italian-invoice-ddt
+ */
+
+namespace ItalianInvoiceDdt;
+
+
+class ItalianInvoiceDdt {
+
+
+	public static function pippobaudo() {
+
+
+		return "che professionista";
+
+
+	}
+
+
+}
